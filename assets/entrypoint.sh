4f1dd2cdcd915406f109044839f6a6fdadbd7d5c
foorschtbar <mail@wischmopp90.com>
2021-02-09 22:39:20 +0100

    Added default values for UID and GID

--- assets/entrypoint.sh
+++ assets/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+# default values for UID and GID
+UID=${UID:-1000}
+GID=${GID:-1000}
+
+# set user and group id for apache user
 usermod -u $UID www-data
 groupmod -g $GID www-data
 
